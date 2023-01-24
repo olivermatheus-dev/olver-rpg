8cbb772d84082a8598b6bec67b8cfd819f8e1918
olivermnoo <riseumpro@gmail.com>
2023-01-23 22:10:57 -0300

    Aperfeiçoamento de Interface e teste da função de ataque.
    Ainda não foram criadas as classes de monstro e character, aqui o progresso é relativo a interface e uma pequena parte do backend. Já consegui fazer a barra de vida do inimigo ser manipulada de acordo com o ataque e o addEventListener do botão de ataque está funcionando.

--- js/game.js
+++ js/game.js
@@ -1,24 +1,31 @@
-// class Game (){
-//     constructor()
-//     {
+function attack() {
+  let hpEnemyValue =
+    document.getElementsByClassName("hpRealEnemy")[0].style.width;
+  if (!hpEnemyValue) {
+    hpEnemyValue = "99%";
+  }
+  let hpEnemyValueNumber = +hpEnemyValue.slice(0, 2);
+  let attackHero = 10;
+  hpEnemyValueNumber -= attackHero;
+  hpEnemyValue = `${hpEnemyValueNumber}%`;
+  console.log(hpEnemyValue);
+  document.getElementsByClassName("hpRealEnemy")[0].style.width = hpEnemyValue;
+  return;
+}
+
+window.addEventListener("load", () => {
+  const attackBtn = document.getElementsByClassName("btn-attack")[0];
+  attackBtn.addEventListener("click", attack);
+});
+
+// class Character(name){
+//     constructor{
+//         this.name = name
+//         this.hp = 100
+//         this.xp = 0
 
 //     }
 
-// genMonster(){
-//     const
-// }
-
-// genCharacter(){
-
-// }
-
-// attack(atacante, alvo){
-//     alvo.hp - atacante.damage
-// }
-
-// buttumAtack.addEventListener("Click", ()=>{
-//     atack()
-
-// })
+// attack()
 
 // }
